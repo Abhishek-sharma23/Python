4fa46223c0e587256cfdcc07795920c4cc93d755
Abhishek sharma <140407169+Abhishek-sharma23@users.noreply.github.com>
2026-02-08 10:38:29 +0530

    Add files via upload

---
+++ Patterns/q9.py
@@ -0,0 +1,8 @@
+n=int(input("Enter Number - "))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if j==1 or j==n or i==j or i+j==n+1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
